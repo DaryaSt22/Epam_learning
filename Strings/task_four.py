924f5b7d59270e998faa62d6bacbf4c2c1f12066
Darya <ara1015@tut.by>
2025-11-21 12:58:08 +0200

    added file task_three (example 4)

--- Strings/task_four.py
+++ Strings/task_four.py
@@ -33,4 +33,16 @@ def check_str(s: str):
             return False
         return True
 
-print(check_str("No 'x' in Nixon"))
+print(check_str("No 'x' in Nixon")) # для автопроверки эту строку лучше убрать
+
+
+def check_str(s: str):
+    """
+    Add your code here
+    """
+    s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+    for i in range(0, len(s) // 2):
+        if s[i] != s[len(s) - i - 1]:
+            return False
+    return True
