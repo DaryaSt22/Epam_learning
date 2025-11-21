e57106b7d8ac6b3afe75e8c25ca8807830d4a18d
Darya <ara1015@tut.by>
2025-11-21 12:39:48 +0200

    added file task_three (example 3)

--- Strings/task_four.py
+++ Strings/task_four.py
@@ -19,3 +19,18 @@ def check_str(s: str):
         return True
 
 print(check_str("malayalam"))
+
+
+
+def check_str(s: str):
+    """
+    Add your code here
+    """
+    s = ''.join(ch.lower() for ch in s if ch.isalnum())
+
+    for i in range(0, len(s) // 2):
+        if s[i] != s[len(s) - i - 1]:
+            return False
+        return True
+
+print(check_str("No 'x' in Nixon"))
