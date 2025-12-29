7f1f796a061a43f20b845e418e2e98a042d86eff
Darya <ara1015@tut.by>
2025-12-29 19:07:55 +0200

    practice arguments task 28

---
+++ Arguments/task_28.py
@@ -0,0 +1,8 @@
+# 28. Смешивание *args и **kwargs
+# Создай функцию show_all(*args, **kwargs), которая сначала печатает args, потом kwargs.
+# Потренируй:
+# show_all(1, 2, 3, a=10, b=20)
+
+
+def show_all(*args, **kwargs):
+    pass
