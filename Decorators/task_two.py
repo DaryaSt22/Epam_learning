ed4279c5eda81e987ca4413de635ab44d1e20644
Darya <ara1015@tut.by>
2026-01-22 11:39:12 +0200

    practice decorators (task_two)

---
+++ Decorators/task_two.py
@@ -0,0 +1,21 @@
+# Functions. Decorators. Decorators. Task 2
+# Write a decorator which logs information about calls of decorated function,
+# values of its arguments, values of keyword arguments and execution time. Log should be written to a file.
+#
+# Example of using
+# @log
+# def foo(a, b, c):
+#     ...
+#
+# foo(1, 2, c=3)
+# log.txt
+# ...
+# foo; args: a=1, b=2; kwargs: c=3; execution time: 0.12 sec.
+# ...
+
+
+def log(fn):
+    """
+    Add your code here or call it from here
+    """
+    pass
