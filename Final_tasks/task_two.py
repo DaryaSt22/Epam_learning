fd138c45d818f0a19c8fc14963220dc5377359cd
Darya <ara1015@tut.by>
2026-02-05 14:41:11 +0200

    final_tasks (task_two)

--- Final_tasks/task_two.py
+++ Final_tasks/task_two.py
@@ -10,8 +10,21 @@
 
 from typing import List
 
+
 def split_by_index(s: str, indexes: List[int]) -> List[str]:
     """
     Add your code here or call it from here
     """
-    pass
+
+    valid_indexes = set()
+
+    for i in indexes:
+        if 0 < i < len(s):
+            valid_indexes.add(i)
+
+    pts = [0] + sorted(valid_indexes) + [None]
+    return [s[i:j] for i, j in zip(pts, pts[1:])]
+
+
+print(split_by_index("pythoniscool,isn'tit?", [6, 6, 8, 8, 12, 13, 18, 18]))
+print(split_by_index("no luck", [42]))
