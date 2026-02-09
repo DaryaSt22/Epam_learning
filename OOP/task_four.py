7011cfadb97236e427a028eb9a3d1a08779b8787
Darya <ara1015@tut.by>
2026-02-09 21:21:50 +0200

    OOP (task_four)

--- OOP/task_four.py
+++ OOP/task_four.py
@@ -9,4 +9,20 @@
 # ["bar"]
 
 class HistoryDict:
-    pass
+
+    def __init__(self, *args, **kwargs):
+        self._data = {}
+        self._history = []
+        if args or kwargs:
+            self._data.update(*args, **kwargs)
+
+
+    def set_value(self, key, value):
+        self._data[key] = value
+
+    def get_history(self):
+        pass
+
+
+d = HistoryDict({"foo": 42})
+print(d._data)
