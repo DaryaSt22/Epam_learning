d05d63e941fba4598c23df9e070786276e69176d
Darya <ara1015@tut.by>
2026-02-09 17:33:57 +0200

    OOP (task_four)

---
+++ OOP/task_four.py
@@ -0,0 +1,10 @@
+# Implement a custom dictionary that will memorize the 5 latest changed keys. Using method "get_history" return these keys.
+#
+# Example:
+#
+# >>> d = HistoryDict({"foo": 42})
+# >>> d.set_value("bar", 43)
+# >>> d.get_history()
+#
+# ["bar"]
+
