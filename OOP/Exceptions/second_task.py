ba031a4eed51b398e29a5803d9eed7cfb6836712
Darya <ara1015@tut.by>
2026-02-18 15:12:50 +0200

    OOP (exceptions, second_task)

--- OOP/Exceptions/second_task.py
+++ OOP/Exceptions/second_task.py
@@ -13,3 +13,16 @@
 # "Error code: invalid literal for int() with base 10: '*'"
 
 
+from typing import Union
+
+
+def divide(str_with_ints: str) -> Union[float, str]:
+    """
+    Returns the result of dividing two numbers or an error message.
+    :arg
+        str_with_ints: str, ex. "4 2";
+    :return
+        result of dividing: float, ex. 2.0 (4 / 2 = 2.0);
+        error response in "Error code: {error message}: str;
+    """
+    raise NotImplementedError('Implement me!')
