bb3d8e4f6a312152931d5c9f7f0ae443d48c55b3
Darya <ara1015@tut.by>
2026-02-19 20:50:04 +0200

    OOP (magic_methods, task_one)

--- OOP/magic_methods/task_one.py
+++ OOP/magic_methods/task_one.py
@@ -8,3 +8,23 @@
 # >>> Counter([1, 2, 3]) + "mississippi"
 #
 # ["1 mississippi", "2 mississippi" , "3 mississippi"]
+
+
+from typing import List
+
+
+class Counter:
+    def __init__(self, values: List[int]):
+        self.values = values
+
+    # TODO: add your code here
+
+    def __add__(self, other):
+        if not isinstance(other, str):
+            return NotImplemented
+
+        result = []
+        for x in self.values:
+            result.append(f"{x} {other}")
+
+        return result
