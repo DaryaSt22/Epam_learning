927314f171157b14a0e677391de44e8fec9b9b4f
Darya <ara1015@tut.by>
2026-02-19 20:50:54 +0200

    OOP (magic_methods, second_task)

---
+++ OOP/magic_methods/second_task.py
@@ -0,0 +1,7 @@
+# Create a hierarchy out of birds. Implement 4 classes:
+#
+# class Bird with an attribute name and methods fly and walk.
+# class FlyingBird with attributes name, ration, and with the same methods. ration must have a default value. Implement the method eat which will describe its typical ration.
+# class NonFlyingBird with same characteristics but which obviously without attribute fly. Add the same "eat" method but with other implementation regarding the swimming bird tastes.
+# class SuperBird which can do all of it: walk, fly, swim and eat. But be careful which "eat" method you inherit.
+# Implement str() function call for each class.
