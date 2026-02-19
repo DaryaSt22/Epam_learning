ffdc9210254e31b3ed55e794ef23bbfd374b46b2
Darya <ara1015@tut.by>
2026-02-19 20:51:16 +0200

    OOP (magic_methods, second_task)

--- OOP/magic_methods/second_task.py
+++ OOP/magic_methods/second_task.py
@@ -5,3 +5,19 @@
 # class NonFlyingBird with same characteristics but which obviously without attribute fly. Add the same "eat" method but with other implementation regarding the swimming bird tastes.
 # class SuperBird which can do all of it: walk, fly, swim and eat. But be careful which "eat" method you inherit.
 # Implement str() function call for each class.
+
+
+class Bird:
+    pass
+
+
+class FlyingBird:
+    pass
+
+
+class NonFlyingBird:
+    pass
+
+
+class SuperBird:
+    pass
