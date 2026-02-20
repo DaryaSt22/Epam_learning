e3bec721445c6f399d00e19e90a81d5103430d1a
Darya <ara1015@tut.by>
2026-02-20 18:06:01 +0200

    OOP (magic_methods, third_task)

--- OOP/magic_methods/third_task.py
+++ OOP/magic_methods/third_task.py
@@ -6,3 +6,38 @@
 # course - classmethod which returns string in the following pattern: {float value} {currency to} for 1 {currency for}
 # to_currency - method transforms currency from one currency to another.
 # Method should return instance of a required currency.
+
+from __future__ import annotations
+from typing import Type
+
+
+class Currency:
+    """
+    1 EUR = 2 USD = 100 GBP
+
+    1 EUR = 2 USD    ;  1 EUR = 100 GBP
+    1 USD = 0.5 EUR  ;  1 USD = 50 GBP
+    1 GBP = 0.02 USD ;  1 GBP = 0.01 EUR
+    """
+
+    def __init__(self, value: float):
+        pass
+
+    @classmethod
+    def course(cls, other_cls: Type[Currency]) -> str:
+        raise NotImplementedError
+
+    def to_currency(self, other_cls: Type[Currency]):
+        raise NotImplementedError
+
+
+class Euro(Currency):
+    pass
+
+
+class Dollar(Currency):
+    pass
+
+
+class Pound(Currency):
+    pass
