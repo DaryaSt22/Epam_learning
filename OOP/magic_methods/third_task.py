d8a04f5215442aa31461aa654d0f0a125805f03f
Darya <ara1015@tut.by>
2026-02-20 18:05:41 +0200

    OOP (magic_methods, third_task)

---
+++ OOP/magic_methods/third_task.py
@@ -0,0 +1,8 @@
+# Description
+# Implement class Currency and inherited classes Euro, Dollar, Pound. Course is 1 EUR == 2 USD == 100 GBP
+#
+# You need to implement the following methods:
+#
+# course - classmethod which returns string in the following pattern: {float value} {currency to} for 1 {currency for}
+# to_currency - method transforms currency from one currency to another.
+# Method should return instance of a required currency.
