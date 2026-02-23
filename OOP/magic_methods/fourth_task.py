f5419ccc51c7ef3846154f3cdedbbd8819805d4d
Darya <ara1015@tut.by>
2026-02-23 21:02:27 +0200

    OOP (magic_methods, fourth_task)

--- OOP/magic_methods/fourth_task.py
+++ OOP/magic_methods/fourth_task.py
@@ -19,3 +19,24 @@
 #
 # >>> b.author = "new author"  # => ValueError: Author can not be changed.
 # >>> b.name = "new name"      # => ValueError: Name can not be changed.
+
+
+class PriceControl:
+    """
+    Descriptor which don't allow to set price
+    less than 0 and more than 100 included.
+    """
+    pass
+
+
+class NameControl:
+    """
+    Descriptor which don't allow to change field value after initialization.
+    """
+    pass
+
+
+class Book:
+    author = NameControl()
+    name = NameControl()
+    price = PriceControl()
