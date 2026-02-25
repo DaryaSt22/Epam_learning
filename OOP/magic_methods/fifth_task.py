ff34eed04e17d48b43e5d755c321c0b324d8b40d
Darya <ara1015@tut.by>
2026-02-25 15:22:57 +0200

    OOP (magic_methods, fifth_task)

--- OOP/magic_methods/fifth_task.py
+++ OOP/magic_methods/fifth_task.py
@@ -1,8 +1,11 @@
 # Create a context manager TempDir (Use Context Manager protocol - methods __enter__, __exit__):
 #
-# When entering the context, a new temporary directory is created with random, unique name. Use os.mkdir to create the directory.
-# Until exiting this context the new created directory becomes current one and all actions are executed in scope of this new directory.
-# When exiting this context, the temporary directory is removed with all files in it. Use rmtree from shutil to remove whole directory.
+# When entering the context, a new temporary directory is created with random,
+# unique name. Use os.mkdir to create the directory.
+# Until exiting this context the new created directory becomes current one
+# and all actions are executed in scope of this new directory.
+# When exiting this context, the temporary directory is removed with all files in it.
+# Use rmtree from shutil to remove whole directory.
 # The new working directory becomes the same as before entering context.
 
 
