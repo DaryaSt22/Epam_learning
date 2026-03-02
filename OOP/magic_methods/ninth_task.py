dac93ba4a2ea6436143f6cf8d7c7239086fdc120
Darya <ara1015@tut.by>
2026-03-02 18:20:26 +0200

    OOP (magic_methods, ninth task)

--- OOP/magic_methods/ninth_task.py
+++ OOP/magic_methods/ninth_task.py
@@ -16,3 +16,11 @@
 # "Kojima is genius"
 
 
+class Cipher:
+    def encode(self, data):
+        pass
+        # TODO: please add your code here
+
+    def decode(self, data):
+        pass
+        # TODO: please add your code here
