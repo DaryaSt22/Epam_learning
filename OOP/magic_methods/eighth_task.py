e8a4cf02d5f5971a531c27e88f6675d19f0f2aa4
Darya <ara1015@tut.by>
2026-03-02 18:14:13 +0200

    OOP (magic_methods, eighth task)

---
+++ OOP/magic_methods/eighth_task.py
@@ -0,0 +1,10 @@
+# A singleton is a class that allows only a single instance of itself to be created and gives access to that created instance. Implement singleton logic inside your custom class using a method to initialize class instance.
+#
+# Example:
+#
+# >>> p = Sun.inst()
+# >>> f = Sun.inst()
+# >>> p is f
+# True
+
+
